3bd8fb24043f2d0260d5f59ea131e27853e93261
divincode <devadiga.1@iitj.ac.in>
2021-04-11 22:23:33 +0530

    New image after new tab

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 function setImage(image) {
     document.body.setAttribute(
       'style',
-      `background-image: url(${image.urls.full});`
+      `background-image: url(${image.base64});`
     );
   }
   
   document.addEventListener('DOMContentLoaded', () => {
-    // Retrieve the next image object
-    chrome.storage.local.get('nextImage', data => {
+    chrome.storage.local.get('nextImage', (data) => {
       if (data.nextImage) {
-      //    console.log(data);
         setImage(data.nextImage);
       }
     });
+  
+  
+    chrome.runtime.sendMessage({ command: 'next-image' });
+  
   });
   
